168294f9907cc97d4438eb5b54d535592413716d
Root Tuquito <root@tuquito.org.ar>
2010-09-13 22:49:56 -0300

    new translations, launcher fixed

--- usr/lib/tuquito/tuquito-update/update-manager.py
+++ usr/lib/tuquito/tuquito-update/update-manager.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 """
- Tuquito Update Manager 1.0-15
+ Tuquito Update Manager 1.0-16
  Copyright (C) 2010
  Author: Mario Colque <mario@tuquito.org.ar>
  Tuquito Team! - www.tuquito.org.ar
